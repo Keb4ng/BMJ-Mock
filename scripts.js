260cd0b3b1d7b305e682987db7a53a279d105e77
Kevo <jontrinidad18@gmail.com>
2025-07-01 17:51:16 +0800

    first commit

--- scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  const orgData = JSON.parse($("#org-data").text());
-
-  console.log(orgData.OrganizationId);
-  console.log(orgData.DeveloperName);
-  console.log(orgData.Url);
-});
